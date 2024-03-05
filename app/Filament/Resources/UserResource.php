30005b52042454abddcba6c211c18f4284e0265b
wbarrera09 <william-barrera1@hotmail.com>
2024-03-04 23:47:00 -0600

    Se agrega CRUD de usuarios y registro para cuentas nuevas v1.2

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Exports\UsersExporter;
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Actions\ExportAction;
+use Filament\Actions\Exports\Models\Export;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\FormsComponent;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\ExportBulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Actions\Exports\Enums\ExportFormat;
+
+
+
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                TextInput::make('name')
+                ->label('Nombre')
+                ->required(),
+                TextInput::make('email')
+                ->label('Correo')
+                ->email()
+                ->required(),
+                TextInput::make('password')
+                ->password()
+                ->required()
+                ->hiddenOn('edit'), 
+            
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+                
+            ->columns([
+                TextColumn::make('name')
+                ->label('Nombre')
+                ->searchable()
+                ->sortable(),
+
+                TextColumn::make('email')
+                ->label('Correo electrónico')
+                ->searchable()
+                ->sortable(),
+                TextColumn::make('created_at')
+                ->label('Creado')
+                ->searchable()
+                ->sortable(),
+
+
+                //Los campos mencionados anteriormente funcionan desde la tabla Users y mostrarlos
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    
+                    ExportBulkAction::make()
+                    ->exporter(UsersExporter::class)
+                    ->formats([
+                        ExportFormat::Csv,
+                    ])
+
+                ]),
+        
+
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -27,8 +28,15 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
+            ->userMenuItems([
+                MenuItem::make()
+                ->label('Admin')
+                ->icon('heroicon-o-cog-6-tooth')
+                ->url('/admin')
+            ])
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
